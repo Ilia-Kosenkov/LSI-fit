fddce04a04e231f9b2a4eaf5adbb7fa2a4ca5714
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-03-20 23:29:34 +0300

    Method anotation

--- LSI-fit/R/kepler_eq.R
+++ LSI-fit/R/kepler_eq.R
@@ -2,7 +2,8 @@
 #'
 #' @param M Mean anomaly (numeric vector)
 #' @param e Eccentricity (scalar numeric value within [0, 1])
-#'
+#' @description Simple Newtonian method applied to the inverse problem.
+#' See https://en.wikipedia.org/wiki/Kepler's_equation#Numerical_approximation_of_inverse_problem
 #' @return Vector of size \code{vec_size(M)} of eccentric anomalies (E)
 solve_E <- function(M, e = 0) {
     e <- vec_cast(vec_assert(e, size = 1L), double())
@@ -13,6 +14,7 @@ solve_E <- function(M, e = 0) {
 
     M <- vec_cast(M, double())
     n <- vec_size(M)
+
     E <- if(e >= 0.8) vec_repeat(pi, n) else M
 
     threshold <- 2 * .Machine$double.eps
@@ -20,6 +22,8 @@ solve_E <- function(M, e = 0) {
     err <- threshold * 10
     counter <- 0L
 
+    # Iterations stop either if the desired precision is reached
+    # or if the number of iterations exceeds 30
     while (err > threshold & counter < 30L) {
         temp <- E - (E - e * sin(E) - M) / (1 - e * cos(E))
         err <- sqrt(sum((E - temp) ^ 2) / n)
