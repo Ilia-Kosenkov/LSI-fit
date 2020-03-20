35ab63b41c2a302672edf1b47e017de8b915f396
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-03-20 23:41:06 +0300

    Rearranged dependencies

--- LSI-fit/R/packages.R
+++ LSI-fit/R/packages.R
@@ -1,23 +1,25 @@
-library(tibble)
-library(dplyr)
-library(tidyr)
-library(magrittr)
-library(purrr)
-library(forcats)
+library(tibble)       # Extended data frames
+library(dplyr)        # Data frames manipulations
+library(tidyr)        # Data "tidying"
+library(magrittr)     # Pipes (%>%, %T>%, %<>%)
+library(purrr)        # Functional-style methods
+library(forcats)      # Dealing with factors
+library(readr)        # Advanced I/O
+library(ggplot2)      # Graphics
+library(stringr)      # String manipulations & regex
 
-library(ggplot2)
-
-library(rlang)
-library(vctrs)
-
-library(RLibs)
-library(primitiveR)
-library(rastro)
-library(sciplotr)
-
-library(drake)
+library(rlang)        # Basic R library
+library(vctrs)        # Basic type library
+                      #
+library(primitiveR)   # Tools to bridge {tidyverse} and {vctrs} in dev mode
+library(rastro)       # {vctrs}-based types like <rastro_degr>
+library(sciplotr)     # Better axes/scales & facet for {ggplot2}
+                      #
+library(drake)        # Execution orchestration & reproducibility
 
 source_all <- function() {
-    require(RLibs)
-    RLibs::source_all("R", "packages.R")
+    fs::path("R") %>%
+        fs::dir_ls(recurse = TRUE, glob = "*.R") %>%
+        discard(str_ends, "packages\\.R") %>%
+        walk(source, encoding = "UTF-8")
 }
