f005f2ed42f29598686de4d0b7c244403856b2d2
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-03-22 15:35:04 +0200

    Increased number of iterations

--- LSI-fit/R/kepler_eq.R
+++ LSI-fit/R/kepler_eq.R
@@ -5,12 +5,12 @@
 #' @description Simple Newtonian method applied to the inverse problem.
 #' See https://en.wikipedia.org/wiki/Kepler's_equation#Numerical_approximation_of_inverse_problem
 #' @return Vector of size \code{vec_size(M)} of eccentric anomalies (E)
-solve_E <- function(M, e = 0) {
+solve_E <- function(M, e = 0, iters = 10L) {
     # Code greatly simplified to work with {greta}
     E <- M
     counter <- 0L
 
-    while (counter < 5L) {
+    while (counter < iters) {
         E <- E - (E - e * sin(E) - M) / (1 - e * cos(E))
         counter <- counter + 1L
     }
