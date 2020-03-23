127366a8d2708ffd66024735d93981f53e76fb84
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-03-23 15:09:41 +0200

    Third model

--- LSI-fit/_drake.R
+++ LSI-fit/_drake.R
@@ -4,7 +4,19 @@ source_all()
 plan <- drake::drake_plan(
     data = import_data(),
     #fit_result = fit_with_greta(data)
-    fit_result_all = fit_with_greta_all(data)
+    #fit_result_all = fit_with_greta_all(data)
+    #fit_result_all_2 = fit_with_greta_all(data,
+            #i = vec_c(0, 100),
+            #e = vec_c(0, 0.35),
+            #lambda_p = vec_c(0, 360),
+            #omega = vec_c(0, 360),
+            #phi_p = vec_c(80, 120)),
+    fit_result_all_3 = fit_with_greta_all(data,
+            i = vec_c(0, 100),
+            e = vec_c(0, 0.35),
+            lambda_p = vec_c(0, 360),
+            omega = vec_c(0, 360),
+            phi_p = vec_c(0, 360))
     )
 
 drake::drake_config(
