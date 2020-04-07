10df20d267050c572fc223f34f1da7e440cc7fc8
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-04-07 20:22:50 +0300

    Generating density plots

--- LSI-fit/R/density_plot.R
+++ LSI-fit/R/density_plot.R
@@ -1,6 +1,13 @@
 breaks <- vec_c("N", "B", "V", "R")
-colors <- vec_c("black", "blue", "green", "red")
-ltys <- vec_c(1, 1, 2, 3)
+colors <- vec_c("#000000", "#377EB8", "#4DAF4A", "#E41A1C")
+ltys <- vec_c(1, 1, 2, 4)
+
+cnt_colors_s <- scales::viridis_pal()(4)
+cnt_colors_g <- vec_c(
+    brewer.pal(4, "OrRd") %>% rev,
+    brewer.pal(4, "BuGn") %>% rev,
+    brewer.pal(4, "Blues") %>% rev,
+    )
 
 group_by_filter <- function(data) {
     data %>%
@@ -11,7 +18,6 @@ group_by_filter <- function(data) {
                 glue_fmt_chr("\\[{1:3},1\\]$")))) %>%
         separate(".var", vec_c(".var", ".filter"), ":", fill = "right") %>%
         mutate(.filter = if_else(is.na(.filter), "N", .filter) %>% as_factor)
-        
 }
 
 generate_pairs <- function(data) {
@@ -19,7 +25,7 @@ generate_pairs <- function(data) {
         pull(.var) %>%
         unique -> params
 
-    params <- params[4:6]
+    #params <- params[3:5]
 
     params %>%
         imap_dfr(~tibble(Row = .x, Col = params[1:.y], RowId = .y, ColId = 1:.y))
@@ -34,9 +40,13 @@ spread_data <- function(data) {
         transmute(.draw, .filter, !!right := .obs)
 
     if (vec_size(left_table) < vec_size(right_table))
-        left_table <- map_dfr(vec_c("B", "V", "R"), ~ mutate(left_table, .filter = .x))
+        left_table <- map_dfr(
+            vec_c("B", "V", "R"),
+            ~ mutate(left_table, .filter = .x))
     if (vec_size(right_table) < vec_size(left_table))
-        right_table <- map_dfr(vec_c("B", "V", "R"), ~ mutate(right_table, .filter = .x))
+        right_table <- map_dfr(
+            vec_c("B", "V", "R"),
+            ~ mutate(right_table, .filter = .x))
 
 
 
@@ -44,24 +54,30 @@ spread_data <- function(data) {
 
 }
 
-generate_1d <- function(data, lims, plot_x_vals) {
+generate_1d <- function(data, lims, plot_x_vals, plot_y_vals = FALSE) {
     data %>%
     ggplot_sci(aes(
             x = .obs,
             col = .filter,
             linetype = .filter,
             group = .filter)) +
-        geom_density() +
+        geom_density(size = 1) +
         scale_x_sci(name = NULL, limits = lims,
             labels =
                 if (vec_cast(plot_x_vals, logical()) %===% TRUE)
                     waiver()
                 else
                     function(x) rep("", vec_size(x)),
-            sec.axis = dup_axis_sci_weak()) +
+            sec.axis = dup_axis_sci_weak(),
+            breaks_n = 3L) +
         scale_y_sci(name = NULL,
-            labels = function(x) rep("", vec_size(x)),
-            sec.axis = dup_axis_sci()) +
+            labels =
+                if (vec_cast(plot_y_vals, logical()) %===% TRUE)
+            waiver()
+            else
+                function(x) rep("", vec_size(x)),
+            sec.axis = sec_axis_sci(~ .),
+            breaks_n = 3L) +
         scale_color_manual(breaks = breaks, values = colors) +
         scale_linetype_manual(breaks = breaks, values = ltys) +
         theme_sci(legend.position = "none")
@@ -71,34 +87,57 @@ generate_2d <- function(data, lims, plot_x_vals, plot_y_vals) {
     data %>%
         spread_data -> data
 
-    names(data) %->% c(.draw, .filter, x_name, y_name)
+    data %>% pull(.filter) %>% unique %>% vec_size %>% equals(1L) -> is_single
 
-    ggplot_sci(data, aes(x = !!sym(x_name), y = !!sym(y_name))) +
-        geom_density2d_sci(breaks = vec_c(0.68, 0.95, 0.997)) +
+    names(data) %->% c(.draw, .filter, x_name, y_name)
+    ggplot_sci(data, aes(
+            x = !!sym(x_name), y = !!sym(y_name))) +
+    #geom_density2d_sci(aes(
+    #col = as_factor(.filter),
+    #group = as_factor(.filter),
+    #linetype = as_factor(.filter)),
+    #size = 1,
+    #lineend = "round",
+    #breaks = 1 - 2 * (1 - pnorm(1:3))) +
+        geom_density2d_filled_sci(aes(
+            group = as_factor(.filter),
+            linetype = as_factor(.filter),
+            fill = ..group..),
+            alpha = 0.75,
+            col = "#000000") +
         scale_x_sci(name = NULL, limits = lims[[x_name]],
             labels =
                 if (vec_cast(plot_x_vals, logical()) %===% TRUE)
                     waiver()
                 else
                     function(x) rep("", vec_size(x)),
-            sec.axis = dup_axis_sci_weak()) +
+            sec.axis = dup_axis_sci_weak(),
+            breaks_n = 3L) +
         scale_y_sci(name = NULL, limits = lims[[y_name]],
             labels =
                 if (vec_cast(plot_y_vals, logical()) %===% TRUE)
                     waiver()
                 else
                     function(x) rep("", vec_size(x)),
-            sec.axis = dup_axis_sci_weak()) +
-        guides(fill = FALSE)
+            sec.axis = dup_axis_sci_weak(),
+            breaks_n = 3L) +
+        #guides(fill = FALSE) +
+        scale_fill_manual(values = if(is_single) cnt_colors_s else cnt_colors_g) +
+        scale_linetype_manual(breaks = breaks, values = ltys) +
+        theme_sci(legend.position = "none")
 }
 
 generate_density <- function(tbl, data, ranges) {
     sz <- tbl %>% pull(Row) %>% unique %>% vec_size
-    mar_1 <- mar_(0.05 ~ cm, 0.05 ~ cm, 0.05 ~ cm, 1 ~ cm)
-    mar_2 <- mar_(0.05 ~ cm, 0.05 ~ cm, 0.5 ~ cm, 1 ~ cm)
+    s_mar <- u_(0.05 ~ cm)
+    l_mar <- u_(0.95 ~ cm)
     pmap(tbl, function(Row, Col, RowId, ColId) {
         if (Row %===% Col)
-            generate_1d(data %>% filter(.var %==% Row), ranges[[Row]], ColId %===% sz)
+            generate_1d(
+                data %>% filter(.var %==% Row),
+                ranges[[Row]],
+                ColId %===% sz,
+                ColId %===% 1L)
         else
             generate_2d(
                 data %>% filter(.var %vin% vec_c(Row, Col)),
@@ -106,40 +145,67 @@ generate_density <- function(tbl, data, ranges) {
                 RowId %===% sz,
                 ColId %===% 1L)
         }) %>%
-        map(ggplot_build %>>% ggplot_gtable) %>%
-        map2(tbl$RowId, ~postprocess_axes(.x,
-            axes_margin = if(.y %===% sz) mar_2 else mar_1,
+        map(ggplot_build %>>% ggplot_gtable) -> grobs
+  
+        pmap(list(grobs, tbl$RowId, tbl$ColId), ~postprocess_axes(.x,
+            axes_margin = mar_(
+                s_mar,
+                if(..3 %===% sz) l_mar else s_mar,
+                0.5 * (if(..2 %===% sz) l_mar else s_mar),
+                if(..3 %===% 1L) l_mar else s_mar),
             text_margin = mar_(0 ~ pt))) -> grobs
 
     m <- make_layout(sz)
     arrangeGrob(grobs = grobs, layout_matrix = m) -> grob
+    delta <- l_mar - s_mar
 
     tbl %>%
         pull(Col) %>%
         unique %>%
-        map(textGrob, x = u_(0.5$npc + 0.5$cm)) -> lower_text_grobs
+        map(fix_names) %>%
+        imap(~ textGrob(.x, x = npc_(0.5) +
+                if (.y %===% 1L)
+                    delta / 2
+                else if (.y %===% sz)
+                    - delta / 2
+                else u_(0$pt),
+            gp = gpar(fontsize = 18))) -> lower_text_grobs
     tbl %>%
         pull(Row) %>%
         unique %>%
-        map(textGrob, rot = 90) -> left_text_grobs
+        map(fix_names) %>%
+        imap(~textGrob(.x, rot = 90, y = npc_(0.5) +
+                if (.y %===% sz) l_mar / 2 else u_(0$pt),
+            gp = gpar(fontsize = 18))) -> left_text_grobs
     grob %>%
-        gtable_add_rows(u_(0.5 ~ cm)) %>%
-        gtable_add_grob(gtable_row("bottom-row", lower_text_grobs), t = sz + 1, l = 1, r = sz) %>%
-        gtable_add_cols(u_(0.5 ~ cm), pos = 0) %>%
-        gtable_add_grob(gtable_col("left-col", left_text_grobs), t = 1, l = 1, b = sz)
+        gtable_add_rows(u_(0.75 ~ cm)) %>%
+        gtable_add_grob(lower_text_grobs, t = sz + 1, l = 1:sz) %>%
+        gtable_add_cols(u_(0.75 ~ cm), pos = 0) %>%
+        gtable_add_grob(left_text_grobs[-1], t = 2:sz, l = 1) -> grob
+
+
+    panel_width <- (u_(1 ~ npc) - 2 * delta - grob$widths[1]) / sz
+    grob$widths[1:sz + 1] <- panel_width
     
+    grob$widths[2] <- grob$widths[2] + delta
+    grob$widths[1 + sz] <- grob$widths[1 + sz] + delta
+
+    panel_height <- (u_(1 ~ npc) - 0.5 * delta - grob$heights[sz + 1]) / sz
+    grob$heights[1:sz] <- panel_height
+    grob$heights[sz] <- grob$heights[sz] + 0.5 * delta
+    grob
 }
 
 fix_names <- function(name) {
     switch(name,
-        "lambda_p" = "$\\lambda_\\mathrm{p}$",
-        "phi_p" = "$\\phi_\\mathrm{p}$",
-        "omega" = "$\\Omega$",
-        "tau[1,1]" = "$\\tau_B$",
-        "tau[2,1]" = "$\\tau_V$",
-        "tau[3,1]" = "$\\tau_R$",
-        glue_fmt_chr("${name}$")
-           )
+        "lambda_p" = "$\\lambda_\\mathrm{p},~^\\circ$",
+        "phi_p" = "$\\phi_\\mathrm{p},~^\\circ$",
+        "omega" = "$\\Omega,~^\\circ$",
+        "tau" = "$f_0$",
+        "i" = "$i,~^\\circ$",
+        "q_0" = "$q_0$,~\\%",
+        "u_0" = "$u_0$,~\\%",
+        glue_fmt_chr("${name}$"))
 
 }
 
@@ -159,27 +225,30 @@ compute_ranges <- function(tbl, data) {
     tbl %>% pull(Row) %>% unique -> vars
     vars %>%
         set_names(.) %>%
-        map(~filter(data, .var %==% .x) %>% pull(.obs) %>% quantile(vec_c(1e-5, 1 - 1e-5)))
+        map(~filter(data, .var %==% .x) %>%
+            pull(.obs) %>%
+            range)
 }
 
-if (!exists("data_tbl"))
-    data_tbl <- read_csv(fs::path("output", "fit_3.csv"), col_types = cols()) %>%
+draw_final_plot <- function(src = drake::readd(fit_3_result_all)) {
+    library(tikzDevice)
+    tictoc::tic("GROB generated")
+    data_tbl <- src %>%
         select(.draw, .var, .obs) %>%
+        mutate(.obs = if_else(
+            .var %vin% vec_c("i", "omega", "lambda_p", "phi_p"),
+            rad_2_deg(.obs), .obs)) %>%
         group_by_filter
 
-#group_by_filter(data_tbl) %>%
-    #filter(.var %vin% vec_c("q_0", "u_0")) %>%
-    #spread_data %>% print
-data_tbl %>% generate_pairs -> tbl
-compute_ranges(tbl, data_tbl) -> ranges
-print(tbl)
+    data_tbl %>% generate_pairs -> tbl
+    compute_ranges(tbl, data_tbl) -> ranges
 
-grid.newpage()
-generate_density(tbl, data_tbl, ranges) %>% grid.draw
-
-
-
-
-# 1. Compute 0.005 - 0.995 ?? limits and apply to all
-# 2. Remove axes from inner plots
-# 3. Change margins to make all plots of same size
+    generate_density(tbl, data_tbl, ranges) -> grob
+    tictoc::toc()
+    tictoc::tic("PDF written")
+    path <- fs::path("output", "posteriors.tex")
+    tikz(path, width = 10 + 1, height = 10 + 0.47, standAlone = TRUE)
+    tryCatch(grid.draw(grob), finally = dev.off())
+    RLibs::tex_2_pdf(path)
+    tictoc::toc()
+}
